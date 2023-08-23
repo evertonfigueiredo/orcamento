17ce8a1dc79e35f7a227d033e20bee9773b35f6e
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-08-23 17:54:26 -0300

    teste

--- server.php
+++ server.php
@@ -5,8 +5,9 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
+echo "$uri";
 if ($uri == "/hook.php") {
-    echo "Teste";
+    
     require_once 'hook.php';
 }else{
     if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
